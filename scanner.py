6daf203f389aa4af13d919560024b747b6facaa3
Twins1 <33881658+Twins1@users.noreply.github.com>
2019-06-16 11:28:06 +0000

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -62,7 +62,6 @@ if resp == '1':
 	print("[+] Ip status: ", scanner[ip_addr].state())
 	print(scanner[ip_addr].all_protocols())
 	print("[+] Open Ports: ", scanner[ip_addr]['tcp'].keys())
-	print("[+] Open Ports: ", scanner[ip_addr]['udp'].keys())
 if resp == '2':
 	print("Nmap Version: ", scanner.nmap_version())
 	scanner.scan(ip_addr, '1-1024', '-v -sS')
@@ -70,7 +69,6 @@ if resp == '2':
 	print("[+] Ip status: ", scanner[ip_addr].state())
 	print(scanner[ip_addr].all_protocols())
 	print("[+] Open Ports: ", scanner[ip_addr]['tcp'].keys())
-	print("[+] Open Ports: ", scanner[ip_addr]['udp'].keys())
 elif resp == '3':
 	print("[+] Nmap Version: ", scanner.nmap_version())
 	scanner.scan(ip_addr, '1-1024', '-v -sU')
@@ -78,7 +76,6 @@ elif resp == '3':
 	print("[+] Ip status: ", scanner[ip_addr].state())
 	print(scanner[ip_addr].all_protocols())
 	print("[+] Open Ports: ", scanner[ip_addr]['udp'].keys())
-	print("[+] Open Ports: ", scanner[ip_addr]['tcp'].keys())
 elif resp == '4':
 	print("[+] Nmap Version: ", scanner.nmap_version())
 	scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
@@ -86,7 +83,6 @@ elif resp == '4':
 	print("[+] Ip status: ", scanner[ip_addr].state())
 	print(scanner[ip_addr].all_protocols())
 	print("[+] Open Ports: ", scanner[ip_addr]['tcp'].keys())
-	print("[+] Open Ports: ", scanner[ip_addr]['udp'].keys())
 elif resp == '5':
 	print("Nmap Version: ", scanner.nmap_version())
 	scanner.scan(ip_addr, '1-1024', '-sV')
@@ -94,7 +90,6 @@ elif resp == '5':
 	print("[+] Ip status: ", scanner[ip_addr].state())
 	print(scanner[ip_addr].all_protocols())
 	print("[+] Open Ports: ", scanner[ip_addr]['tcp'].keys())
-	print("[+] Open Ports: ", scanner[ip_addr]['udp'].keys())
 elif resp >= '6':
 	print('[-] Please enter a valid option!')
 	sys.exit()
